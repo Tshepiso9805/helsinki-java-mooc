0673b3c3af2fb282c73ade99783ef4e3c8d19d84
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-08 15:42:28 +0200

    Conversation

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -6,6 +6,14 @@ public class Conversation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Greetings! How are you doing?");
+        String response1 = scanner.nextLine();
+
+        System.out.println("Oh, how interesting. Tell me more!");
+        String response2 = scanner.nextLine();
+
+        System.out.println("Thanks for sharing!");
+
         // Write your program here
 
     }
