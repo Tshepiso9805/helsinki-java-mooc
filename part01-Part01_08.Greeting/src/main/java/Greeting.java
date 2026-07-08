6986c9177aceae3ad7d519b193ae749587e20cd9
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-08 15:29:53 +0200

    Greeting

--- part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -6,6 +6,11 @@ public class Greeting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("What's your name?");
+
+        String answer = scanner.nextLine();
+        System.out.println("Hi " + answer);
+
         // Write your program here
 
     }
