b3c3b7c73e66e35c1ac2b83eecd6856a30559aca
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-08 15:08:49 +0200

    HiAdaLovelace

--- part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -2,7 +2,8 @@
 public class HiAdaLovelace {
 
     public static void main(String[] args) {
-        String name = "Ada Lovelace";
+        String name = "Ada Lovelace!";
+        System.out.println("Hi "+ name);
 
 
     }
