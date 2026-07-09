d921341681fc6b67bd8713109d51dc4a79144e91
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-09 14:57:17 +0200

    SecInADay

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -5,6 +5,12 @@ public class SecondsInADay {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many days would you like to convert to seconds? ");
+
+        int inp = Integer.valueOf(scanner.nextLine());
+        int secInADay = 86400;
+        int total = inp * secInADay;
+        System.out.println(total);
 
         // Write your program here
 
