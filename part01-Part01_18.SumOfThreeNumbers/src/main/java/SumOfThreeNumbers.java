e779bab098b2b50fe39543646ce12f48129d247f
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-09 15:28:41 +0200

    SumOf2Num

--- part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -6,6 +6,17 @@ public class SumOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give the first number:");
+        int firstN = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secN = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (firstN + secN + third));
+
         // Write your program here
 
     }
