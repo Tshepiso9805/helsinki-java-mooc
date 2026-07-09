9e1d20f6841d3677781cd1667cef67133520d7af
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-09 18:59:14 +0200

    simpleCal

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -6,6 +6,18 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give the first number:");
+        int firstN = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secN = Integer.valueOf(scanner.nextLine());
+
+
+        System.out.println(firstN +" + " + secN + " = " + (firstN + secN));
+        System.out.println(firstN +" - " + secN + " = " + (firstN - secN));
+        System.out.println(firstN +" * " + secN + " = " + (firstN * secN));
+        System.out.println(firstN +" / " + secN + " = " + (1.0 *firstN / secN));
+
         // Write your program here
 
     }
