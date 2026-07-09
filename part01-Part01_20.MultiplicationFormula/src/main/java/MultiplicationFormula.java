afd0038764d879f1c5bfd9901377dcadc60d18af
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-09 18:08:08 +0200

    multFormular

--- part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -5,6 +5,15 @@ public class MultiplicationFormula {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstN = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secN = Integer.valueOf(scanner.nextLine());
+
+        int sum = firstN * secN;
+
+        System.out.println(firstN +" * " + secN + " = " + sum);
 
         // Write your program here
 
