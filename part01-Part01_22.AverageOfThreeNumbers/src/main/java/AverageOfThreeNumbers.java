cb3f48ddbd43be599dc96723b57563117be2d92c
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-09 18:45:15 +0200

    AveOf3Num

--- part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -5,6 +5,19 @@ public class AverageOfThreeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstN = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secN = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scanner.nextLine());
+
+        double ave = (1.0 * firstN + secN + third)/3;
+
+        System.out.println("The average is "+ave);
+
 
         // Write your program here
 
