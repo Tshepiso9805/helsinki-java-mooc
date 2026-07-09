32f3c94ff18874586c19f86296b7745fb976131d
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-09 18:30:52 +0200

    AveOf2Num

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -5,6 +5,15 @@ public class AverageOfTwoNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstN = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secN = Integer.valueOf(scanner.nextLine());
+
+        double ave = (1.0 * firstN + secN)/2;
+
+        System.out.println("The average is "+ave);
 
         // Write your program here
 
