75bd8ed5ab245c1531d3d32bded5e90c5d02c651
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-15 15:47:13 +0200

    AveOfPositiveNums

--- part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -5,11 +5,6 @@ public class SumOfTwoNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the first number:");
-        int firstN = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int secN = Integer.valueOf(scanner.nextLine());
-        System.out.println("The sum of the numbers is " + (firstN + secN));
 
         // Write your program here
 
---
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+
+        while (true){
+            int numbers = Integer.valueOf(scanner.nextLine());
+
+            if (numbers < 0){
+                continue;
+            }else if (numbers > 0){
+                count++;
+                sum += numbers;
+                continue;
+            } else if (numbers == 0) {
+                break;
+
+            }
+        }
+
+        if (count == 0){
+            System.out.println("Cannot calculate the average");
+        }else {
+            double ave = (double) sum/ count;
+            System.out.println(ave);
+        }
+
+    }
+}
