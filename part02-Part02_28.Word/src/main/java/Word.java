739d97e0b45e171c4755e30cc76bf3b4736cb1ce
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-16 23:46:41 +0200

    Word

--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 public class Word {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        word();
 
+
+    }
+    public static String word(){
+        return "Naledi";
     }
 
 }
