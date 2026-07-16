c32703aef22825915ca457acdbe79a8acd485257
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-16 10:20:37 +0200

    CountingToHunfred

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,12 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int start = Integer.valueOf(scanner.nextLine());
+        int end = 100;
+
+        for (int t = start; t <= end; t++){
+            System.out.println(t);
+        }
+
     }
 }
