1cbaac4524c2ab53fbb0eeff3694fcf4426313ca
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-30 14:30:52 +0200

    YourFirstAccount

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account myFirstAcc = new Account("Tshepiso", 100.00);
+        myFirstAcc.deposit(20.00);
+        System.out.println(myFirstAcc);
+    }
+}
