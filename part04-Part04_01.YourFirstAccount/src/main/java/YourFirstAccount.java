a0572758b54e093004985fe69829d88f5a781522
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-07-30 15:01:24 +0200

    DogAttributes

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,7 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
+
         // Write your program here
         Account myFirstAcc = new Account("Tshepiso", 100.00);
         myFirstAcc.deposit(20.00);
