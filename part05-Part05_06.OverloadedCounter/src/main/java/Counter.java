81191de9b2aa37a221585a6e4ddd4cc04602e39f
Tshepiso <tshsekon021@student.wethinkcode.co.za>
2026-08-19 11:08:13 +0200

    OverloadedCounter

---
+++ part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
@@ -0,0 +1,31 @@
+public class Counter {
+    private int value;
+
+    public  Counter(int startValue){
+        this.value = startValue;
+    }
+    public Counter(){
+        this(0);
+    }
+
+    public int value(){
+        return value;
+    }
+    public void increase(){
+        this.value = value + 1;
+    }
+    public void decrease(){
+        this.value = value - 1;
+    }
+    public void increase(int increaseBy){
+        if (increaseBy > 0){
+            this.value = value + increaseBy;
+        }
+
+    }
+    public void decrease(int decreaseBy){
+        if (decreaseBy > 0){
+            this.value = value - decreaseBy;
+        }
+    }
+}
